76d8d6e45ff2b3776d37185ec3a26ac09c162faa
BugsCleaners <91881471+BugsCleaners@users.noreply.github.com>
2022-04-24 10:59:55 +0300

    add

---
+++ ASP.NET Core/Basic/SimpleWebAPI/WebAPICore/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPICore.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("hello")]
+        public IActionResult hello(string? name = "World")
+        {
+                return new OkObjectResult( String.Format("Hello {0}", name));
+           
+        }
+
+    
+
+    }
+}
